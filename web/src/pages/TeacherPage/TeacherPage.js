364efc771a019f012355510a3902c6e624be8334
Ryan Doyle <ryandoyle@hey.com>
2023-06-26 10:39:13 -0700

    add teacher home

--- web/src/pages/TeacherPage/TeacherPage.js
+++ web/src/pages/TeacherPage/TeacherPage.js
@@ -8,16 +8,30 @@ const TeacherPage = () => {
 
   return (
     <>
-      <MetaTags title="Teacher" description="Teacher page" />
-
-      <h1>TeacherPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/TeacherPage/TeacherPage.js</code>
-      </p>
-      <p>
-        My default route is named <code>teacher</code>, link to me with `
-        <Link to={routes.teacher()}>Teacher</Link>`
-      </p>
+      <MetaTags title="Teacher" description="Teacher Home" />
+      <div className="w-full">
+        <h1 className="text-xl my-6">
+          {currentUser?.firstName
+            ? `Welcome, ${currentUser.firstName}!`
+            : 'Welcome!'}
+        </h1>
+        <div className="px-4 flex flex-col sm:flex-row justify-between">
+          <div className="mb-6 sm:w-1/2">
+            <p>Select a group to start awarding kudos!</p>
+          </div>
+          <div className="sm:w-1/2">
+            <ul className="nes-list is-disc mb-6 pl-4">
+              <li className="mb-2">Group 1</li>
+              <li className="mb-2">Group 2</li>
+              <li className="mb-2">Group 3</li>
+              <li className="mb-2">Group 4</li>
+            </ul>
+            <button className="nes-btn is-success place-self-end">
+              Add Group
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
