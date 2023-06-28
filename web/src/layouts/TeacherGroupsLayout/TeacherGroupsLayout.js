026f49650ff4b44a27eb2bd2aa3e7480f6bcd1fb
Ryan Doyle <ryandoyle@hey.com>
2023-06-28 09:16:10 -0700

    home button routing

--- web/src/layouts/TeacherGroupsLayout/TeacherGroupsLayout.js
+++ web/src/layouts/TeacherGroupsLayout/TeacherGroupsLayout.js
@@ -1,17 +1,49 @@
 import { NavLink, routes } from '@redwoodjs/router'
 
+import { useAuth } from 'src/auth'
+
 const TeacherGroupLayout = ({ children, groupId }) => {
+  const { isAuthenticated, hasRole } = useAuth()
+
+  const HomeButton = () => {
+    if (isAuthenticated && !hasRole('TEACHER')) {
+      return (
+        <NavLink
+          className="nes-btn is-primary"
+          activeClassName="is-success"
+          to={routes.student()}
+        >
+          Home
+        </NavLink>
+      )
+    }
+    if (isAuthenticated && hasRole('TEACHER')) {
+      return (
+        <NavLink
+          className="nes-btn is-primary"
+          activeClassName="is-success"
+          to={routes.teacher()}
+        >
+          Home
+        </NavLink>
+      )
+    }
+    return (
+      <NavLink
+        className="nes-btn is-primary"
+        activeClassName="is-success"
+        to={routes.teacher()}
+      >
+        Home
+      </NavLink>
+    )
+  }
+
   return (
     <>
       <div className="flex w-full justify-center gap-x-8 mb-8">
         <button>
-          <NavLink
-            className="nes-btn is-primary"
-            activeClassName="is-success"
-            to={routes.home()}
-          >
-            Home
-          </NavLink>
+          <HomeButton />
         </button>
         <button>
           <NavLink
