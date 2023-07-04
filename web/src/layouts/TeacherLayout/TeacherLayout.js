19b8e6457365d2e7cc2e15aae4b77cd65df297eb
Ryan Doyle <ryandoyle@hey.com>
2023-07-03 20:55:34 -0700

    remove student links

--- web/src/layouts/TeacherLayout/TeacherLayout.js
+++ web/src/layouts/TeacherLayout/TeacherLayout.js
@@ -1,50 +1,19 @@
-import { NavLink, routes, useLocation } from '@redwoodjs/router'
+import { NavLink, routes } from '@redwoodjs/router'
 
-import { useAuth } from 'src/auth'
 import TeacherGroupHeaderCell from 'src/components/TeacherGroupHeaderCell/TeacherGroupHeaderCell'
 
 const TeacherLayout = ({ children, groupId }) => {
-  const { isAuthenticated, hasRole } = useAuth()
-
-  const HomeButton = () => {
-    if (isAuthenticated && !hasRole('TEACHER')) {
-      return (
-        <NavLink
-          className="nes-btn is-primary"
-          activeClassName="is-success"
-          to={routes.student()}
-        >
-          Home
-        </NavLink>
-      )
-    }
-    if (isAuthenticated && hasRole('TEACHER')) {
-      return (
-        <NavLink
-          className="nes-btn is-primary"
-          activeClassName="is-success"
-          to={routes.teacher()}
-        >
-          Home
-        </NavLink>
-      )
-    }
-    return (
-      <NavLink
-        className="nes-btn is-primary"
-        activeClassName="is-success"
-        to={routes.teacher()}
-      >
-        Home
-      </NavLink>
-    )
-  }
-
   return (
     <>
       <div className="flex w-full justify-center gap-x-8 mb-4">
         <button>
-          <HomeButton />
+          <NavLink
+            className="nes-btn is-primary"
+            activeClassName="is-success"
+            to={routes.teacher()}
+          >
+            Home
+          </NavLink>
         </button>
         <button>
           <NavLink
