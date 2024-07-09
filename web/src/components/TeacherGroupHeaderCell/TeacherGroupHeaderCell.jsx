d05046d76f7c7f346f23b4f126d1e09b79b53113
Ryan Doyle <ryandoyle@hey.com>
2024-07-09 13:04:06 -0700

    copy enrollID to clipboard

--- web/src/components/TeacherGroupHeaderCell/TeacherGroupHeaderCell.jsx
+++ web/src/components/TeacherGroupHeaderCell/TeacherGroupHeaderCell.jsx
@@ -1,3 +1,5 @@
+import { toast } from '@redwoodjs/web/toast'
+
 export const QUERY = gql`
   query TeacherGroupHeaderQuery($id: String!) {
     TeacherGroupHeader: group(id: $id) {
@@ -14,16 +16,16 @@ export const QUERY = gql`
 `
 
 export const Loading = () => (
-  <div className="nes-container flex justify-between mb-4">
+  <div className="nes-container mb-4 flex justify-between">
     <div className="flex flex-col">
       <span className="text-2xl">Loading Group...</span>
-      <span className="text-xs nes-text text-gray-400 hover:text-gray-800">
+      <span className="nes-text text-xs text-gray-400 hover:text-gray-800">
         Enroll ID: loading...
       </span>
     </div>
     <div className="flex">
       <i className="nes-icon coin is-medium"></i>
-      <div className="flex flex-col ml-4">
+      <div className="ml-4 flex flex-col">
         <span className="text-3xl">...</span>
         <span className="text-xs">
           <i>total kudos</i>
@@ -40,17 +42,24 @@ export const Failure = ({ error }) => (
 )
 
 export const Success = ({ TeacherGroupHeader }) => {
+  const handleCopy = () => {
+    navigator.clipboard.writeText(TeacherGroupHeader.enrollId)
+    toast.success('Copied Enroll ID!')
+  }
   return (
-    <div className="nes-container flex justify-between mb-4">
+    <div className="nes-container mb-4 flex justify-between">
       <div className="flex flex-col">
         <span className="text-2xl">{TeacherGroupHeader.name}</span>
-        <span className="text-xs nes-text text-gray-400 hover:text-gray-800">
+        <button
+          className="nes-text text-xs text-gray-400 hover:text-gray-800"
+          onClick={handleCopy}
+        >
           Enroll ID: {TeacherGroupHeader.enrollId}
-        </span>
+        </button>
       </div>
       <div className="flex">
         <i className="nes-icon coin is-medium"></i>
-        <div className="flex flex-col ml-4">
+        <div className="ml-4 flex flex-col">
           <span className="text-3xl">{TeacherGroupHeader.awardedPoints}</span>
           <span className="text-xs">
             <i>total kudos</i>
