81415b52b0188652c1c4b89aa06e0b97d6c124c3
Ryan Doyle <ryandoyle@hey.com>
2024-07-26 14:17:28 -0700

    add multi-award feature across frontend

--- web/src/components/TeacherGroupEnrolledCell/TeacherGroupEnrolledCell.jsx
+++ web/src/components/TeacherGroupEnrolledCell/TeacherGroupEnrolledCell.jsx
@@ -1,4 +1,7 @@
-import { NavLink, routes } from '@redwoodjs/router'
+import { NavLink, routes, useMatch } from '@redwoodjs/router'
+
+import { useSelectedContext } from 'src/components/Context/SelectedEnrolledContext'
+import TeacherGroupEnrolledListLink from 'src/components/TeacherGroupEnrolledListLink/TeacherGroupEnrolledListLink'
 
 export const QUERY = gql`
   query EnolledUsersQuery($id: String!) {
@@ -45,33 +48,65 @@ export const Failure = ({ error }) => (
   <div style={{ color: 'red' }}>Error: {error?.message}</div>
 )
 
-export const Success = ({ enrolledUsers }) => {
+export const Success = ({ id, enrolledUsers }) => {
+  const isMulti = useMatch('/teacher/group/{id}/multi').match
+  const selected = useSelectedContext()
+
+  const handleClick = (enrollment) => {
+    selected.toggleSelectedUser(enrollment)
+  }
+
   return (
-    <ul className="nes-container with-title h-full px-4 pb-3 pr-0">
-      <span className="nes-text title relative -top-2">Students</span>
-      <div className="h-full overflow-y-scroll pr-1">
-        {enrolledUsers.map((enrollment) => {
-          return (
-            <NavLink
-              className="nes-text"
-              activeClassName="nes-text is-primary"
-              key={enrollment.id}
-              to={routes.teacherGroupStudent({
-                id: enrollment.groupId,
-                studentId: enrollment.userId,
+    <div className="nes-container with-title h-full p-0">
+      <span className="nes-text title absolute left-4 top-4">Students</span>
+
+      <div className="h-full overflow-hidden">
+        <div className="my-4 flex w-full justify-between gap-4 px-4">
+          <NavLink
+            className={`nes-btn title relative`}
+            activeClassName={'is-success'}
+            to={routes.teacherGroup({ id: id })}
+          >
+            Single
+          </NavLink>
+          <NavLink
+            className={`nes-btn title relative`}
+            activeClassName={'is-success'}
+            to={routes.teacherGroupStudents({ id: id })}
+          >
+            Multi
+          </NavLink>
+        </div>
+        <ul className="relative h-full overflow-y-scroll pl-4">
+          {isMulti
+            ? enrolledUsers.map((enrollment) => {
+                return (
+                  <li key={enrollment.id}>
+                    <button
+                      onClick={() => {
+                        handleClick(enrollment)
+                      }}
+                      className="inline flex w-full justify-between pb-3 pr-2"
+                    >
+                      <div>
+                        {enrollment?.user?.firstName}{' '}
+                        {enrollment?.user?.lastName}
+                      </div>
+                      <div>{enrollment?.points}</div>
+                    </button>
+                  </li>
+                )
+              })
+            : enrolledUsers.map((enrollment) => {
+                return (
+                  <TeacherGroupEnrolledListLink
+                    key={enrollment.id}
+                    enrollment={enrollment}
+                  />
+                )
               })}
-            >
-              <li className="mb-3 flex justify-between">
-                <p className="inline-block">
-                  {enrollment.user.firstName} {enrollment.user.lastName}
-                  {/* {enrollment.user.lastName.slice(0, 1)}. */}
-                </p>
-                <p className="mr-2 inline-block">{enrollment.points}</p>
-              </li>
-            </NavLink>
-          )
-        })}
+        </ul>
       </div>
-    </ul>
+    </div>
   )
 }
