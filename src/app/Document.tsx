ecc227fb74c0504dd8a84fcc5007ec2b240ba3fd
Ryan Doyle <ryandoyle@hey.com>
2025-08-03 20:05:51 -0700

    add favicons

--- src/app/Document.tsx
+++ src/app/Document.tsx
@@ -10,6 +10,12 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({
       <title>@redwoodjs/starter-standard</title>
       <link rel="modulepreload" href="/src/client.tsx" />
       <link rel="stylesheet" href={styles} />
+
+      <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+      <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
     </head>
     <body>
       <div id="root">{children}</div>
