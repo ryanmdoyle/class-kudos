5d80decac6804dcde8a1532e66cbb4dbf3fb47bb
Ryan Doyle <ryandoyle@hey.com>
2025-10-05 21:56:27 -0700

    render table with dates on client

--- src/app/pages/teacher/TravelLog.tsx
+++ src/app/pages/teacher/TravelLog.tsx
@@ -1,14 +1,7 @@
 import { TeacherNav } from "@/app/components/teacher/TeacherNav"
 import { RequestInfo } from "rwsdk/worker";
 import { db } from "@/db";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/app/components/ui/table"
+import { TravelLogTable } from '@/app/components/teacher/TravelLogTable'
 
 
 export async function TravelLog({ params, request }: RequestInfo) {
@@ -32,10 +25,6 @@ export async function TravelLog({ params, request }: RequestInfo) {
     }
   })
 
-  // const group = await db.group.findUnique({
-  //   where: { id: groupId }
-  // })
-
   return (
     <div className="flex flex-col min-h-screen min-w-screen">
       <div className="h-[100px] flex-shrink-0">
@@ -44,40 +33,7 @@ export async function TravelLog({ params, request }: RequestInfo) {
       <div className="flex-1 overflow-auto flex flex-col gap-4 bg-green-background min-w-screen p-8">
         <div className="bg-background w-full neo-container p-6 mb-4">
           <h2 className="text-2xl font-bold mb-2">Travel Log</h2>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Name</TableHead>
-                <TableHead>Location</TableHead>
-                <TableHead>Time</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {trips.map((trip) => {
-                const arrivedAtStr = typeof trip.arrivedAt === "string"
-                  ? trip.arrivedAt
-                  : trip.arrivedAt.toISOString();
-
-                const safeDate = arrivedAtStr.endsWith("Z") || arrivedAtStr.includes("+")
-                  ? arrivedAtStr
-                  : arrivedAtStr + "Z";
-
-                return (
-                  <TableRow key={trip.id} className="bg-background">
-                    <TableCell>{trip.user.firstName} {trip.user.lastName}</TableCell>
-                    <TableCell>{trip.location.name}</TableCell>
-                    <TableCell className="font-base">{new Date(safeDate).toLocaleString("en-US", {
-                      month: "short",  // Aug
-                      day: "numeric",  // 23
-                      hour: "numeric", // 12
-                      minute: "2-digit", // 32
-                      hour12: true,   // AM/PM
-                    })}</TableCell>
-                  </TableRow>
-                )
-              })}
-            </TableBody>
-          </Table>
+          <TravelLogTable trips={trips} />
         </div>
       </div>
     </div>
